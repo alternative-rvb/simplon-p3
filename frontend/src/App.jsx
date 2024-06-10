8fd7212fad845ceb601f6383cba11e1c63d988ba
Nicolas <alternative.rvb@gmail.com>
2024-06-10 18:34:30 +0200

    test fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  console.log('items:', items)
+
+  useEffect(() => {
+    fetch("http://localhost:5000/items")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <p>
+        {items && items.map((item) => <p key={item.id}>{item.title}</p>)}
+      </p>
     </div>
   );
 }
